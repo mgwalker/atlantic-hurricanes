bd483ee262c9659a9ae4b8fffeac0b7211a0e27c
Greg Walker <mgwalker@gmail.com>
2021-08-28 20:07:10 -0500

    fix git bug; run update

--- .github/workflows/git.sh
+++ .github/workflows/git.sh
@@ -5,7 +5,8 @@ then
   echo "committing and pushing changes"
   git config --local user.email "mgwalker@users.noreply.github.com"
   git config --local user.name "automatic update"
-  git commit -am "regular update"
+  git add .
+  git commit -m "automatic update"
   git push "https://mgwalker:$GITHUB_TOKEN@github.com/$GITHUB_REPOSITORY" HEAD:main
 else
   echo "no changes detected; skipping"
