55b40dd499a8ead65119f33f67695331a41a0a18
Greg Walker <git@greg.email>
2026-08-09 13:18:43 -0500

    start switching over to templates; add charts

---
+++ src/templates/index.js
@@ -0,0 +1,43 @@
+import Handlebars from "handlebars";
+import fs from "node:fs/promises";
+import path from "node:path";
+
+const registeredTemplates = {
+  index: "index.html.handlebars",
+  activeStorm: "active-storm.html.handlebars",
+  finishedStorm: "finished-storm.html.handlebars",
+  stormData: "storm-data.json.handlebars",
+  timeSeriesCharts: "timeseries-charts.html.handlebars",
+};
+
+const templatePromise = Promise.all(
+  Object.entries(registeredTemplates).map(async ([name, filename]) => {
+    const template = await fs.readFile(
+      path.join(import.meta.dirname, filename),
+      {
+        encoding: "utf-8",
+      }
+    );
+    return [name, template];
+  })
+)
+  .then((all) =>
+    all.map(([name, template]) => {
+      Handlebars.registerPartial(name, template);
+      return [name, Handlebars.compile(template)];
+    })
+  )
+  .then((all) =>
+    all.reduce((obj, [name, template]) => ({ ...obj, [name]: template }), {})
+  );
+
+Handlebars.registerHelper("json", function (context) {
+  return JSON.stringify(this.series);
+});
+
+const render = async (context) => {
+  const templates = await templatePromise;
+  return templates.index(context);
+};
+
+export default render;
--- src/web.js
+++ src/web.js
@@ -1,7 +1,7 @@
 import fs from "fs/promises";
 import sqlite from "sqlite3";
 import { fileURLToPath } from "url";
-
+import render from "./templates/index.js";
 import {
   dataPath,
   sqlite as dbUtils,
@@ -56,19 +56,52 @@ export const makeWeb = async () => {
           last.movement_direction_degrees
         )}`,
         deltaWind: prev
-          ? `<br>${delta(
+          ? `${delta(
               last.maximum_sustained_wind_mph,
               prev.maximum_sustained_wind_mph
             )} mph since last update`
           : "",
         deltaPressure: prev
-          ? `<br>${delta(
+          ? `${delta(
               last.minimum_central_pressure_mb,
               prev.minimum_central_pressure_mb
             )} mb since last update`
           : "",
         updated: last.timestamp,
+        series: {
+          pressure: storm.map(({ timestamp, minimum_central_pressure_mb }) => ({
+            timestamp,
+            value: minimum_central_pressure_mb,
+          })),
+          wind: storm.map(({ timestamp, maximum_sustained_wind_mph }) => ({
+            timestamp,
+            value: maximum_sustained_wind_mph,
+          })),
+          extents: {
+            tropical: storm.map(
+              ({ timestamp, hurricane_wind_extent_miles }) => ({
+                timestamp,
+                value: hurricane_wind_extent_miles,
+              })
+            ),
+            hurricane: storm.map(
+              ({ timestamp, tropical_storm_wind_extent_miles }) => ({
+                timestamp,
+                value: tropical_storm_wind_extent_miles,
+              })
+            ),
+          },
+        },
       };
+
+      // If there aren't any tropical storm wind extents, remove it
+      if (!stormData.series.extents.tropical.some(({ value }) => value)) {
+        delete stormData.series.extents.tropical;
+      }
+      // Same with hurricane.
+      if (!stormData.series.extents.hurricane.some(({ value }) => value)) {
+        delete stormData.series.extents.hurricane;
+      }
 
       storms.push(stormData);
     })
@@ -89,214 +122,223 @@ export const makeWeb = async () => {
 
   const active = storms.filter(({ final }) => !final);
 
-  const html = `
-<!DOCTYPE html>
-<html lang="en">
-  <head>
-    <title>Atlantic Hurricanes, ${year}</title>
-    <!-- COMMON TAGS -->
-    <meta charset="utf-8" />
-    <!-- Search Engine -->
-    <meta name="description" content="Atlantic Hurricanes, ${year}" />
-    <meta name="image" content="https://mgwalker.github.io/atlantic-hurricanes/" />
-    <!-- Schema.org for Google -->
-    <meta itemprop="name" content="Atlantic Hurricanes, ${year}" />
-    <meta itemprop="description" content="Atlantic Hurricanes, ${year}" />
-    <!-- Twitter -->
-    <meta name="twitter:card" content="summary" />
-    <meta name="twitter:title" content="Atlantic Hurricanes, ${year}" />
-    <meta name="twitter:description" content="Atlantic Hurricanes, ${year}" />
-    <!-- Open Graph general (Facebook, Pinterest & Google+) -->
-    <meta name="og:title" content="Atlantic Hurricanes, ${year}" />
-    <meta name="og:description" content="Atlantic Hurricanes, ${year}" />
-    <meta name="og:url" content="https://mgwalker.github.io/atlantic-hurricanes/" />
-    <meta name="og:site_name" content="Atlantic Hurricanes, ${year}" />
-    <meta name="og:locale" content="en_US" />
-    <meta name="og:type" content="website" />
+  const html = await render({
+    year,
+    active,
+    multipleActive: active > 1,
+    storms,
+  });
 
-    <style type="text/css">
-      body {
-        font-family: monospace;
-        padding: 0 3em;
-      }
+  //   const html = `
+  // <!DOCTYPE html>
+  // <html lang="en">
+  //   <head>
+  //     <title>Atlantic Hurricanes, ${year}</title>
+  //     <!-- COMMON TAGS -->
+  //     <meta charset="utf-8" />
+  //     <!-- Search Engine -->
+  //     <meta name="description" content="Atlantic Hurricanes, ${year}" />
+  //     <meta name="image" content="https://mgwalker.github.io/atlantic-hurricanes/" />
+  //     <!-- Schema.org for Google -->
+  //     <meta itemprop="name" content="Atlantic Hurricanes, ${year}" />
+  //     <meta itemprop="description" content="Atlantic Hurricanes, ${year}" />
+  //     <!-- Twitter -->
+  //     <meta name="twitter:card" content="summary" />
+  //     <meta name="twitter:title" content="Atlantic Hurricanes, ${year}" />
+  //     <meta name="twitter:description" content="Atlantic Hurricanes, ${year}" />
+  //     <!-- Open Graph general (Facebook, Pinterest & Google+) -->
+  //     <meta name="og:title" content="Atlantic Hurricanes, ${year}" />
+  //     <meta name="og:description" content="Atlantic Hurricanes, ${year}" />
+  //     <meta name="og:url" content="https://mgwalker.github.io/atlantic-hurricanes/" />
+  //     <meta name="og:site_name" content="Atlantic Hurricanes, ${year}" />
+  //     <meta name="og:locale" content="en_US" />
+  //     <meta name="og:type" content="website" />
 
-      h2 .updated {
-        color: #888;
-        font-size: 0.8rem;
-        margin: 0;
-      }
+  //     <link rel="stylesheet" type="text/css" href="http://suddenlygreg.com/static-assets/main.css">
 
-      table {
-        width: 100%;
-      }
+  //     <style type="text/css">
+  //       body {
+  //         font-family: monospace;
+  //         padding: 0 3em;
+  //       }
 
-      table th.spacer, table td.spacer {
-        padding-left: 2em;
-      }
+  //       h2 .updated {
+  //         color: #888;
+  //         font-size: 0.8rem;
+  //         margin: 0;
+  //       }
 
-      table td.storm {
-        border-top: 4px solid black;
-        border-bottom: 1px solid #bbb;
-      }
+  //       table {
+  //         width: 100%;
+  //       }
 
-      table td.storm.category-1 {
-        background-color: #ffffcc;
-      }
+  //       table th.spacer, table td.spacer {
+  //         padding-left: 2em;
+  //       }
 
-      table td.storm.category-2 {
-        background-color: #ffe775;
-      }
+  //       table td.storm {
+  //         border-top: 4px solid black;
+  //         border-bottom: 1px solid #bbb;
+  //       }
 
-      table td.storm.category-3 {
-        background-color: #ffc140;
-      }
+  //       table td.storm.category-1 {
+  //         background-color: #ffffcc;
+  //       }
 
-      table td.storm.category-4 {
-        background-color: #ff8f20;
-      }
+  //       table td.storm.category-2 {
+  //         background-color: #ffe775;
+  //       }
 
-      table td.storm.category-4 h2 .updated {
-        color: #eee;
-      }
+  //       table td.storm.category-3 {
+  //         background-color: #ffc140;
+  //       }
 
-      table td.storm.category-5 {
-        background-color: #ff6060;
-      }
+  //       table td.storm.category-4 {
+  //         background-color: #ff8f20;
+  //       }
 
-      table td.storm.category-5 h2 .updated {
-        color: #ddd;
-      }
+  //       table td.storm.category-4 h2 .updated {
+  //         color: #eee;
+  //       }
 
-      table th, table td {
-        text-align: left;
-        vertical-align: top;
-      }
+  //       table td.storm.category-5 {
+  //         background-color: #ff6060;
+  //       }
 
-      table th + th, table td + td {
-        padding-left: 1em;
-      }
+  //       table td.storm.category-5 h2 .updated {
+  //         color: #ddd;
+  //       }
 
-      figcaption {
-        font-size: 0.7em;
-        text-align: right;
-      }
-    </style>
-  </head>
-  <body>
-    <h1>Atlantic Storms, ${year}</h1>
+  //       table th, table td {
+  //         text-align: left;
+  //         vertical-align: top;
+  //       }
 
-    <p>
-      Data of all storms:
-      <a href="https://raw.githubusercontent.com/mgwalker/atlantic-hurricanes/main/data/storms.${year}.sqlite">sqlite</a>
-    </p>
+  //       table th + th, table td + td {
+  //         padding-left: 1em;
+  //       }
 
-    ${
-      active.length < 2
-        ? ""
-        : `
-    <h2>${active.length} active storms</h2>
-    <img src="active.png">`
-    }
+  //       figcaption {
+  //         font-size: 0.7em;
+  //         text-align: right;
+  //       }
+  //     </style>
+  //   </head>
+  //   <body>
+  //     <h1>Atlantic Storms, ${year}</h1>
 
-    <table>${storms
-      .map(
-        (storm) => `
-      <tr>
-        <td colspan="6" class="storm ${
-          storm.final ? "" : `category-${storm.category}`
-        }" ${
-          storm.final
-            ? `style="cursor:pointer;" onClick="toggle('${storm.id}')"`
-            : ""
-        }>
-          <h2>${
-            storm.final
-              ? `🏁 ${storm.name}`
-              : `
-            ${storm.classification} ${storm.name}${
-                  storm.category > 0 ? ` - Category ${storm.category}` : ""
-                }
-            <p class="updated">last updated <span data-time>${
-              storm.updated
-            }</span></p>`
-          }
-          </h2>
-        </td>
-      </tr>
-      ${
-        storm.final
-          ? ""
-          : `
-      <tr>
-        <th class="spacer"></th>
-        <th>wind speed</th>
-        <th>central pressure</th>
-        <th>movement</th>
-        <th>data (CSV)</th>
-        <th>National Hurricane Center</th>
-      </tr>
-      <tr>
-        <td class="spacer"></td>
-        <td>${storm.maximum_sustained_wind_mph} mph${storm.deltaWind}</td>
-        <td>${storm.minimum_central_pressure_mb} mb${storm.deltaPressure}</td>
-        <td>
-          ${storm.heading}
-          <br/>
-          <span style="display: inline-block; font-size: 1.5em; transform: rotate(${
-            storm.movement_direction_degrees
-          }deg);">↑</span>
-        </td>
-        <td><a href="https://raw.githubusercontent.com/mgwalker/atlantic-hurricanes/main/data/csv/${
-          storm.id
-        }.csv">${storm.id}.csv</a></td>
-        <td><a href="https://www.nhc.noaa.gov/archive/${year}/${storm.name.toUpperCase()}.shtml?">NHC advisories</a></td>
-      </tr>`
-      }
-      <tr id="storm_${storm.id}" ${storm.final ? 'style="display:none;"' : ""}>
-        <td class="spacer">
-        <td colspan="5">
-          <figure>
-            <img src="${storm.id}.png">
-            <figcaption>
-              Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors,
-              Imagery &copy; <a href="https://www.mapbox.com/">Mapbox</a> |
-              <a href="https://www.mapbox.com/map-feedback/" target="_blank">Improve this map</a>
-            </figcaption>
-          </figure>
-        </td>
-      </tr>
-      `
-      )
-      .join("\n")}
-    </table>
-    <script type="text/javascript">
-      const formatter = new Intl.DateTimeFormat([], {
-        day: "numeric",
-        hour: "numeric",
-        minute: "numeric",
-        month: "long",
-        timeZoneName: "short",
-        weekday: "long",
-        year: "numeric"
-      });
+  //     <p>
+  //       Data of all storms:
+  //       <a href="https://raw.githubusercontent.com/mgwalker/atlantic-hurricanes/main/data/storms.${year}.sqlite">sqlite</a>
+  //     </p>
 
-      Array.from(document.querySelectorAll("[data-time]")).forEach((e) => {
-        const d = new Date(Date.parse(e.innerText));
-        e.innerText = formatter.format(d);
-      });
+  //     ${
+  //       active.length < 2
+  //         ? ""
+  //         : `
+  //     <h2>${active.length} active storms</h2>
+  //     <img src="active.png">`
+  //     }
 
-      const toggle = (id) => {
-        const el = document.querySelector(\`#storm_\${id}\`);
-        if(el.style.display === "none") {
-          el.style.display = "";
-        } else {
-          el.style.display = "none";
-        }
-      }
-    </script>
-  </body>
-</html>
-`;
+  //     <table>${storms
+  //       .map(
+  //         (storm) => `
+  //       <tr>
+  //         <td colspan="6" class="storm ${
+  //           storm.final ? "" : `category-${storm.category}`
+  //         }" ${
+  //           storm.final
+  //             ? `style="cursor:pointer;" onClick="toggle('${storm.id}')"`
+  //             : ""
+  //         }>
+  //           <h2>${
+  //             storm.final
+  //               ? `🏁 ${storm.name}`
+  //               : `
+  //             ${storm.classification} ${storm.name}${
+  //                   storm.category > 0 ? ` - Category ${storm.category}` : ""
+  //                 }
+  //             <p class="updated">last updated <span data-time>${
+  //               storm.updated
+  //             }</span></p>`
+  //           }
+  //           </h2>
+  //         </td>
+  //       </tr>
+  //       ${
+  //         storm.final
+  //           ? ""
+  //           : `
+  //       <tr>
+  //         <th class="spacer"></th>
+  //         <th>wind speed</th>
+  //         <th>central pressure</th>
+  //         <th>movement</th>
+  //         <th>data (CSV)</th>
+  //         <th>National Hurricane Center</th>
+  //       </tr>
+  //       <tr>
+  //         <td class="spacer"></td>
+  //         <td>${storm.maximum_sustained_wind_mph} mph${storm.deltaWind}</td>
+  //         <td>${storm.minimum_central_pressure_mb} mb${storm.deltaPressure}</td>
+  //         <td>
+  //           ${storm.heading}
+  //           <br/>
+  //           <span style="display: inline-block; font-size: 1.5em; transform: rotate(${
+  //             storm.movement_direction_degrees
+  //           }deg);">↑</span>
+  //         </td>
+  //         <td><a href="https://raw.githubusercontent.com/mgwalker/atlantic-hurricanes/main/data/csv/${
+  //           storm.id
+  //         }.csv">${storm.id}.csv</a></td>
+  //         <td><a href="https://www.nhc.noaa.gov/archive/${year}/${storm.name.toUpperCase()}.shtml?">NHC advisories</a></td>
+  //       </tr>`
+  //       }
+  //       <tr id="storm_${storm.id}" ${storm.final ? 'style="display:none;"' : ""}>
+  //         <td class="spacer">
+  //         <td colspan="5">
+  //           <figure>
+  //             <img src="${storm.id}.png">
+  //             <figcaption>
+  //               Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors,
+  //               Imagery &copy; <a href="https://www.mapbox.com/">Mapbox</a> |
+  //               <a href="https://www.mapbox.com/map-feedback/" target="_blank">Improve this map</a>
+  //             </figcaption>
+  //           </figure>
+  //         </td>
+  //       </tr>
+  //       `
+  //       )
+  //       .join("\n")}
+  //     </table>
+  //     <script type="text/javascript">
+  //       const formatter = new Intl.DateTimeFormat([], {
+  //         day: "numeric",
+  //         hour: "numeric",
+  //         minute: "numeric",
+  //         month: "long",
+  //         timeZoneName: "short",
+  //         weekday: "long",
+  //         year: "numeric"
+  //       });
+
+  //       Array.from(document.querySelectorAll("[data-time]")).forEach((e) => {
+  //         const d = new Date(Date.parse(e.innerText));
+  //         e.innerText = formatter.format(d);
+  //       });
+
+  //       const toggle = (id) => {
+  //         const el = document.querySelector(\`#storm_\${id}\`);
+  //         if(el.style.display === "none") {
+  //           el.style.display = "";
+  //         } else {
+  //           el.style.display = "none";
+  //         }
+  //       }
+  //     </script>
+  //   </body>
+  // </html>
+  // `;
 
   fs.writeFile(`${docsPath}/index.html`, html);
 };
